4e6b6947c2d0280a5d4e77c9ade0c1bd1cea28cc
D1scak3 <miguel.fazenda2000@gmail.com>
2022-05-30 22:20:07 +0100

    teste where added

--- RetrieveService/test/test_main.py
+++ RetrieveService/test/test_main.py
@@ -7,37 +7,26 @@ test_app = TestClient(app)
 
 
 @mock_s3
-def test_files_correct_file():
+def test_return_file_names_and_size():
     """
-    Given a file
-    When the user calls the endpoint
-    Then it saves the file in the bucket.
-    """
-    
-    conn = boto3.resource('s3', region_name='us-east-1')
-    conn.create_bucket(Bucket='tdg-s3-bucket')
-
-    response = test_app.post(
-        "/api/files", files={"upload_file": open("main.py", "rb")}
-    )
-    assert response.status_code == 200
-
-@mock_s3
-def test_files_override_file():
-    """
-    Given a file
-    When there is a already a file with that name saved in the bucket
-    Then the file should be overwritten.
+    Given a user
+    When he request the templates that have been stored in the S3
+    The the API should return their file names and size
+    :return:
     """
 
     conn = boto3.resource('s3')
     conn.create_bucket(Bucket='tdg-s3-bucket')
 
     s3 = boto3.client('s3')
-    s3.put_object(Bucket='tdg-s3-bucket', Key="main.py", Body="test")
-    
-    response = test_app.post(
-        "/api/files", files={"upload_file": open("main.py", "rb")}
+    s3.put_object(Bucket='tdg-s3-bucket', Key="text1.xlsx", Body="test")
+    s3.put_object(Bucket='tdg-s3-bucket', Key="text2.docx", Body="test")
+
+    response = test_app.get(
+        "/api/2/files"
     )
+
     assert response.status_code == 200
+    assert response.json()["data"][0]["Name"] == "text1.xlsx"
+    assert response.json()["data"][1]["Name"] == "text2.docx"
 
