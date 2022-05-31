c228b0bf74d2b54e4b5cc927eafcc2c7a3cae7da
leand12 <leandrosilva12@ua.pt>
2022-05-31 18:22:36 +0100

    feedback to user when storage is empty

--- frontend/src/pages/ListTemplates.js
+++ frontend/src/pages/ListTemplates.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // material
 import { Card, CardHeader, Box, Button } from '@mui/material';
 import Table from '@mui/material/Table';
@@ -15,50 +15,52 @@ import FileService from 'src/services/FileService';
 const service = FileService.getInstance();
 
 export default function ListTemplates() {
+    const [rows, setRows] = useState(null);
 
-    const [rows, setRows] = React.useState([]);
-
-    React.useEffect(() => {
+    useEffect(() => {
         service.getAllFiles()
             .then(res => res.json())
-            .then(res => {console.log(res); setRows(res.data);})
-            .catch(_ => setRows([]))
-
+            .then(res => { console.log(res); setRows(res.data); })
+            .catch(_ => setRows([]));
     }, []);
-
 
     return (
         <Card>
             <CardHeader title={"List Files"} subheader={"See your files"} />
             <Box sx={{ p: 3, pb: 1, minHeight: 300, display: 'flex', alignItems: 'center', justifyContent: 'center' }} dir="ltr">
-                {rows.length != 0 ?
-                    <TableContainer component={Paper}>
-                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell style={{width: "80%"}} align="left">Name</TableCell>
-                                    <TableCell style={{width: "15%"}} align="right">Size</TableCell>
-                                    <TableCell style={{width: "5%"}} align="left"></TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {rows.map((row) => (
-                                    <TableRow
-                                        key={row.Name}
-                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                    >
-                                        <TableCell style={{width: "80%"}} component="th" scope="row">
-                                            {row.Name}
-                                        </TableCell>
-                                        <TableCell style={{width: "15%"}}  align="right">{row.Size}</TableCell>
-                                        <TableCell style={{width: "5%"}}  align="left"></TableCell>
-
+                {rows === null ?
+                    <CircularProgress />
+                    :
+                    rows?.length === 0 ?
+                        <h2 style={{color: "#637381"}}>Your files storage is empty.</h2>
+                        :
+                        <TableContainer component={Paper}>
+                            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell style={{ width: "80%" }} align="left">Name</TableCell>
+                                        <TableCell style={{ width: "15%" }} align="right">Size</TableCell>
+                                        <TableCell style={{ width: "5%" }} align="left"></TableCell>
                                     </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                : <CircularProgress />}
+                                </TableHead>
+                                <TableBody>
+                                    {rows.map((row) => (
+                                        <TableRow
+                                            key={row.Name}
+                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                        >
+                                            <TableCell style={{ width: "80%" }} component="th" scope="row">
+                                                {row.Name}
+                                            </TableCell>
+                                            <TableCell style={{ width: "15%" }} align="right">{row.Size}</TableCell>
+                                            <TableCell style={{ width: "5%" }} align="left"></TableCell>
+
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                }
             </Box>
         </Card>
     )
