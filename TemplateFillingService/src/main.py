460ee41767a21a1f100cfbdd83007bb09255ddc5
D1scak3 <miguel.fazenda2000@gmail.com>
2022-06-01 23:37:30 +0100

    strategy defined, kinda...

--- TemplateFillingService/src/main.py
+++ TemplateFillingService/src/main.py
@@ -6,8 +6,13 @@ import boto3
 
 import json
 import openpyxl as oxl
+import docx
 
 from utils import create_response, get_client_s3
+
+# TODO: remove this import after done
+import os
+import re
 
 app = FastAPI()
 bucket_name = "tdg-s3-bucket"
@@ -21,12 +26,11 @@ logging.basicConfig(
 
 @app.post("/api/3/fill")
 async def post_template(
-    upload_file: UploadFile = File(...),
-    retrieval_filename: str = Form(),
-    output_filename: str = Form(),
-    s3=Depends(get_client_s3)
+        upload_file: UploadFile = File(...),
+        retrieval_filename: str = Form(),
+        output_filename: str = Form(),
+        s3=Depends(get_client_s3)
 ) -> JSONResponse:
-
     """
     Endpoint ``/fill`` that accepts the method POST. Receives a JSON file
     and a template name.
@@ -69,7 +73,6 @@ async def post_template(
 
 
 def fill_template(template_name, data, filled_file_name):
-
     # load template
     template = oxl.load_workbook(template_name)
 
@@ -91,3 +94,59 @@ def fill_template(template_name, data, filled_file_name):
         i += 1
 
     template.save(filled_file_name)
+
+
+def fill_simple_docx_template(template_name, data, filled_file_name):
+    # open file
+    document = docx.Document(template_name)
+
+    # regex
+
+    # paragraph input
+    regex = re.compile("\$\{([a-zA-Z0-9]+)\}")
+    for paragraph in document.paragraphs:
+        while x := re.search(regex, paragraph.text):
+            replace = x.string.split(" ")[-1]
+            strip = replace[2:-1]
+            if strip in data[0]:
+                paragraph.text = paragraph.text.replace(replace, data[0][strip])
+
+    # table input
+    start_list_regex = re.compile("\$\{\#([a-zA-Z0-9]+)\}")
+    list_regex = re.compile("\$\{([a-zA-Z0-9]+)\}")
+    end_list_regex = re.compile("\$\{([a-zA-Z0-9]+)\#\}")
+    for table in document.tables:
+        for row in table.rows:
+            # TODO
+            # get correct list
+            # get list length
+            # create right amount of rows
+            # fill rows with data
+
+            # this probably isn't right
+            for cell in row.cells:
+                for paragraph in cell.paragraphs:
+                    # replace list init
+                    start_list_replace = re.search(start_list_regex, paragraph.text)
+                    paragraph.text = paragraph.text.replace(start_list_replace, "")
+
+                    # replace list element
+                    replace = re.search(list_regex, paragraph.text)
+                    paragraph.text = paragraph.text.replace(replace, data[0][replace[2:-1]])
+
+                    # replace list end
+                    end_list_replace = re.search(end_list_regex, paragraph.text)
+                    paragraph.text = paragraph.text.replace(end_list_replace, "")
+
+
+    document.save("../Templates/" + filled_file_name)
+
+
+if __name__ == "__main__":
+    print(os.path.dirname(os.path.realpath(__file__)))
+
+    f = open("../Templates/1_template_simpleWord/input_simpleWord-Data.json")
+    data = json.load(f)
+
+    fill_simple_docx_template("../Templates/1_template_simpleWord/input_simpleWord-Template.docx", data,
+                              "test-result.docx")
