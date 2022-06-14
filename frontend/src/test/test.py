9dd730903f3fbe985a8f31090030220c306d1de2
inespl <ines.pl@ua.pt>
2022-06-14 17:56:44 +0100

    testes feitos

---
+++ frontend/src/test/test.py
@@ -0,0 +1,37 @@
+from os import PRIO_PGRP
+from matplotlib import use
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from test_upload import TestUpload
+from test_testlogin import TestTestlogin
+from test_testfill import TestTestfill
+
+def main():
+    try:
+        t1 = TestTestlogin()
+        t1.setup_method()
+        t1.test_testlogin()
+        t1.teardown_method
+    except:
+        print("Upload Test Failed")
+    
+    try:
+        t2 = TestUpload()
+        t2.setup_method()
+        t2.test_o()
+        t2.teardown_method()
+    except:
+        print("Upload Test Failed")
+    
+    try:
+        t3 = TestTestfill()
+        t3.setup_method()
+        t3.test_testfill()
+        t3.teardown_method()
+    except:
+        print("Fill Test Failed")
+    
+main()
+
+
