be0829d0c7e6e4b2a2e777f1488eb6dce3074265
inespl <ines.pl@ua.pt>
2022-06-14 21:53:29 +0100

    cleaner code 4

--- frontend/src/test/test.py
+++ frontend/src/test/test.py
@@ -9,7 +9,7 @@ def main():
         t1.setup_method()
         t1.test_testlogin()
         t1.teardown_method
-    except:
+    except Exception:
         print("Upload Test Failed")
 
     try:
@@ -17,7 +17,7 @@ def main():
         t2.setup_method()
         t2.test_o()
         t2.teardown_method()
-    except:
+    except Exception:
         print("Upload Test Failed")
 
     try:
@@ -25,7 +25,7 @@ def main():
         t3.setup_method()
         t3.test_testfill()
         t3.teardown_method()
-    except:
+    except Exception:
         print("Fill Test Failed")
 
 
